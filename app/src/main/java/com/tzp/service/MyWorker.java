2901cbedc43a7a80c6a39453d7722436c049cbed
tanvi <ptanvir@artificial-soft.com>
2023-08-06 09:38:10 +0600

    "Work Manager"

--- app/src/main/java/com/tzp/service/MyWorker.java
+++ app/src/main/java/com/tzp/service/MyWorker.java
@@ -1,11 +1,17 @@
 package com.tzp.service;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MyWorker extends Worker {
     public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
@@ -16,6 +22,25 @@ public class MyWorker extends Worker {
     @Override
     public Result doWork() {
         Log.d("work", "service running....");
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+        }
+        Locations.getLocation(getApplicationContext()).getProviderClient().getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            Log.d("task", "Latitude: " + location.getLatitude() + "\nLatitude: " + location.getLongitude());
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("task", e.getLocalizedMessage());
+                    }
+                });
         return Result.success();
     }
 }
